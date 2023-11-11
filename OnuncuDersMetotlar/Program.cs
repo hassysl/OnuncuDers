123a9869430c81e607c246156b7183c9ac68afc8
egitmen <egitmen@DESKTOP-16QG8HH>
2023-11-11 17:51:20 +0300

    Add project files.

---
+++ OnuncuDersMetotlar/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnuncuDersMetotlar
+{
+    internal class Program
+    {
+        #region Açıklamalar
+        //Metot nasıl oluşturulur:
+
+        //ErişimBelirleyici static MetotTipi MetotAdi()
+        //{
+        //}
+
+        //Erişim Belirleyiciler => public, private, internal, protected gibi değerler alabilen erişim belirleyiciler metotların, değişkenlerin, class'ların vs. başına koyulur.
+
+        //Kullandığımız erişim belirleyiciye göre oluşturduğumuz yapının nerelerden çağırılıp kullanılabileceğini belirleriz.
+
+        //public => public olan yapılar projenin her yerinden çağırılabilir.
+        //private => private olan yapılar sadece oluşturulduğu class üzerinde kullanılabilir.
+
+        //Metot Tipleri => int, string gibi değişken tipleri olabileceği gibi void, liste veya class tipinde de bir metot oluşturulabilir.
+
+        //Bir metot ismi oluşturulurken verdiğimiz ismin ilk harfi mutlaka büyük harfli olmalıdır.
+        #endregion
+
+        #region Metot Oluşturma Türleri
+        //Bir metodun geriye değer döndürmemesini istiyorsak, void tipinde oluştururuz.
+        //Bir metodun geriye değer döndürmesini istiyorsak, void tipinde oluşturmayız.
+
+        //1.Geriye değer döndürmeyen parametre almayan
+        //2.Geriye değer döndürmeyen parametre alan
+        //3.Geriye değer döndüren parametre almayan
+        //4.Geriye değer döndüren parametre alan
+        #endregion
+
+        #region 1.Geriye değer döndürmeyen parametre almayan
+
+        public static void BilgiGetir()
+        {
+            Console.WriteLine("BilgiGetir metodu çalıştı");
+        }
+
+        public static void KullaniciBilgisiAl()
+        {
+            string ad, soyad;
+
+            Console.WriteLine("Adınızı girin:");
+            ad = Console.ReadLine();
+
+            Console.WriteLine("Adınızı girin:");
+            soyad = Console.ReadLine();
+
+            Console.WriteLine($"Hoşgeldin {ad} {soyad}");
+        }
+
+        public static void KonsoluTemizle()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+        #endregion
+
+        #region 2.Geriye değer döndürmeyen parametre alan
+        public static void Topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine($"Sayıların toplamı : {toplam}");
+            //Console.WriteLine("Sayıların toplamı : " + toplam);
+        }
+       
+        public static void Listele(List<string> liste)
+        {
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //Parametre olara verilen listenin elemanlarının toplamını bulan bir metot yazınız.
+
+        public static void ListeElemanlariniTopla(List<int> liste)
+        {
+            int toplam = 0;
+
+            foreach (var item in liste)
+            {
+                toplam = toplam + item;
+            }
+            Console.WriteLine("Toplam : " + toplam);
+
+        }
+
+        #endregion
+
+
+        #region 3.Geriye değer döndüren parametre almayan
+        //Geriye değer döndüren metotların en sonunda mutlaka return anahtar kelimesi kullanılmalıdır.
+        public static string IsimDondur()
+        {
+            Console.WriteLine("Adınız nedir?");
+            string ad = Console.ReadLine();
+
+            return ad;
+        }
+
+        public static int BuyukSayiyiDondur()
+        {
+            Console.WriteLine("İki adet sayı girin:");
+
+            int s1 = int.Parse(Console.ReadLine());
+            int s2 = int.Parse(Console.ReadLine());
+
+            if (s1 > s2)
+            {
+                return s1;
+            }
+            else if (s1 < s2)
+            {
+                return s2;
+            }
+            else
+            {
+                Console.WriteLine();
+                return 0;
+            }
+
+        }
+
+
+
+        #endregion
+        static void Main(string[] args)
+        {
+            //BilgiGetir();
+
+            //KonsoluTemizle();
+
+            //KullaniciBilgisiAl();
+
+            //KonsoluTemizle();
+
+            //Topla(8, -22);
+
+            //KonsoluTemizle();
+
+            //List<string> isimler = new List<string>() { "Ali", "Veli", "Can", "Fatih" };
+
+            //Listele(isimler);
+
+            //KonsoluTemizle();
+
+            //List<int> tekSayilar = new List<int>() { 3, 5, 7, 9};
+            //List<int> ciftSayilar = new List<int>() { 2, 4, 6, 8 };
+
+            //Console.WriteLine("Tek sayılar için :"); 
+            //ListeElemanlariniTopla(tekSayilar);
+
+            //Console.WriteLine("\nÇift sayılar için :");
+            //ListeElemanlariniTopla(ciftSayilar);
+
+            //KonsoluTemizle();
+
+            //string isim = IsimDondur();
+
+            //Console.Write("Hoşgeldin " + isim);
+
+            //KonsoluTemizle();
+
+
+            int sonuc = BuyukSayiyiDondur();
+
+            if (sonuc == 0)
+            {
+                Console.WriteLine("Eşit sayılar girdiniz!");
+            }
+            else
+            {
+                Console.WriteLine(sonuc);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//System.IO => dosyaya veri yazma (StreamWriter), dosyadan veri çekme (StreamReader)
+namespace OnuncuDers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>(); //Boş liste oluşturma
+
+            isimler.Add("Halil");
+            isimler.Add("Şevval");
+            isimler.Add("Ali");
+
+
+            List<string> hayvanlar = new List<string>() //Listeyi dolu oluşturma
+            {
+                "Arı", "Kedi", "Köpek"
+            };
+
+
+            //For döngüsü ile listenin içine eleman ekleme
+
+            List<int> sayilar = new List<int>();
+            //0, 10, 20, 30, 40
+
+            for (int i = 0; i < 5; i++) //Bu döngü 5 defa çalışacak. i = 0, 1, 2, 3, 4
+            {
+                sayilar.Add(i * 10);
+            }
+
+            //Foreach döngüsü ile listenin elemanlarını yazdırma
+
+            foreach (var sayi in sayilar)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+
+            //Bir listenin içine kullanıcı tarafından değer atma işlemi
+            List<string> ulkeler = new List<string>();
+
+            Console.WriteLine("Kaç tane ülke girişi yapmak istersiniz?");
+            int adet = int.Parse(Console.ReadLine()); //"5" => 5
+
+            for (int i = 0; i < adet; i++) //0, 1, 2
+            {
+                Console.WriteLine((i + 1) + ". ülkeyi girin:");
+                ulkeler.Add(Console.ReadLine());
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+
+
+            //Random komutunu kullanarak bir listenin içerisine 5 adet 0 ile 100 arasından rastgele (çift) sayılar atan ve bu sayılar listeleyen program yazınız. Ayrıca bu sayıların toplamlarını da bulalım.
+
+            //1. Boş liste oluşturun.
+            //2. For döngüsü ile listenizi rastgele 5 adet çift sayılar ile doldurun.
+            //3. Foreach ile listenizin çıktısını alın. 
+
+            List<int> sayilarListesi = new List<int>();
+            Random random = new Random();
+            int rastgeleSayi = 0, toplam = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                //Rastgele oluşturulan sayı tek olduğu müddetçe, çift olana kadar tekrardan oluşturulsun.
+                do
+                {
+                    rastgeleSayi = random.Next(0, 100);
+                } while (rastgeleSayi % 2 == 1);
+
+                sayilarListesi.Add(rastgeleSayi);
+            }
+
+            foreach (var sayi in sayilarListesi)
+            {
+                Console.WriteLine(sayi);
+                toplam = toplam + sayi; // toplam += sayi;
+            }
+
+            Console.WriteLine("\nListedeki sayıların toplamı : " + toplam);
+
+            Console.ReadKey();
+            Console.Clear();
+
+
+            //Generic List Metotları
+            //listeAdi.MetotAdi(varsa parametre burada kullanılır);
+
+            List<string> urunler = new List<string>() { "Laptop", "Telefon", "Monitor", "Mouse" };
+
+            //listeAdi.IndexOf(eleman) => Bu metot içerisine vermiş olduğumuz elemanın, listede kaçıncı indexte bulunduğunu gösterir.
+
+            int urunIndexNo = urunler.IndexOf("Telefon");
+            Console.WriteLine("Telefon ürününün index numarası : " + urunIndexNo);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            //listeAdi.Contains(eleman) => Bu metot içerisine vermiş olduğumuz elemanın, listede bulunup bulunmadığını bize boolean tipinde gösterir.
+            Console.Write("Aramak istediğiniz ürünü giriniz: ");
+            string aranacakUrun = Console.ReadLine();
+
+            bool varMi = urunler.Contains(aranacakUrun);
+
+            if (varMi == true)
+            {
+                Console.WriteLine(aranacakUrun + " listede mevcuttur.");
+            }
+            else
+            {
+                Console.WriteLine(aranacakUrun + " listede mevcut değildir.");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            //listeAdi.Insert(index, eleman) => Bu metot listemizin içerisine belirlediğimiz indexe belirlediğimiz elemanı eklememizi sağlar.
+            //Eğer belirttiğimiz indexte eleman var ise, var olan bu eleman bir sonraki indexe atanır.
+
+            urunler.Insert(3, "Klavye");
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun);
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            //listeAdi.Remove(eleman) => Bu metodun içine verdiğimiz eleman var ise listeden silinir.
+            bool silindiMi1 = urunler.Remove("Monitor");
+
+            //listeAdi.RemoveAt(indexNo) => Bu metodun içindeki index numarasına denk gelen eleman var ise silinir.
+            urunler.RemoveAt(0);
+
+            foreach (var item in urunler)
+            {
+                Console.WriteLine(item);
+            }
+
+            //listeAdi.Clear() => Bu metot listenin tüm elemanlarını siler.
+            urunler.Clear();
+
+
+
+            Console.Read();
+        }
+    }
+}
